575e5e58e33d5c52a013075e7e9178bd4d1ca5b4
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-01-12 14:04:02 +0300

    using context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
 import Modal from "./components/Modal";
-import PlayGround from "./components/PlayGround";
+import PlayGroundManager from "./components/PlayGroundManager";
+import { GameProvider } from "./contexts/GameContext";
 
 function App() {
   return (
     <div className="flex h-screen flex-col items-center bg-backgroundGradient py-16 font-barlow">
-      <Header />
-      <PlayGround />
-      <Modal></Modal>
+      <GameProvider>
+        <Header />
+        <PlayGroundManager />
+        <Modal></Modal>
+      </GameProvider>
     </div>
   );
 }
--- src/components/PlayGround.jsx
+++ src/components/PlayGround.jsx
@@ -1,62 +1,31 @@
-import React, { useEffect, useState } from "react";
-import rock from "../../images/icon-rock.svg";
-import paper from "../../images/icon-paper.svg";
-import scissors from "../../images/icon-scissors.svg";
+import React, { useContext } from "react";
+
 import triangle from "../../images/bg-triangle.svg";
+import { GameContext } from "../contexts/GameContext";
 
 function PlayGround() {
-  const [userChoice, setUserChoice] = useState(null);
-  const [choices, setChoices] = useState([
-    {
-      name: "paper",
-      image: paper,
-      styling: "border-paperGradient hover:border-hoverpaperGradient",
-    },
-    {
-      name: "rock",
-      image: rock,
-      styling:
-        "border-rockGradient translate-y-52 hover:border-hoverrockGradient",
-    },
-    {
-      name: "scissors",
-      image: scissors,
-      styling: "border-scissorsGradient hover:border-hoverscissorsGradient",
-    },
-  ]);
-  const handleUserChoice = (choice) => {
-    setChoices([
-      {
-        name: choice.name,
-        image: choice.image,
-        styling: `${choice.styling} duration-700 -translate-x-[calc(100%)] scale-150`,
-      },
-    ]);
+  const { setState, choices } = useContext(GameContext);
 
-    setUserChoice(choice);
-  };
-  useEffect(() => {
-    console.log(choices);
-  }, [choices]);
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="mb-20 flex w-screen justify-evenly text-3xl font-bold text-white">
-        <p className="-mr-[300px]">You Picked</p>
-        <p className="">The House PICKED</p>
-      </div>
       <div className="flex items-center justify-center -space-x-8">
         {choices.map((choice) => (
           <button
             key={choice.name}
             className={`z-20 rounded-full border-[20px] bg-white p-10 text-white transition-all ${choice.styling}`}
-            onClick={() => handleUserChoice(choice)}
+            onClick={() =>
+              setState((prevState) => ({
+                ...prevState,
+                playerChoice: choice,
+              }))
+            }
           >
             <img className="size-16" src={choice.image} alt={choice.name} />
           </button>
         ))}
       </div>
       <img
-        className={`absolute translate-y-28 ${userChoice ? "opacity-0" : "opacity-100"}`}
+        className={`absolute translate-y-28`}
         src={triangle}
         alt="triangle"
       />
