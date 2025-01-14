92a1afb0456e1311b207ecac0a276b4391cc5a5b
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-01-14 12:03:30 +0300

    enhance logo responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import ScoreDisplay from "./ScoreDisplay";
 function Header() {
   return (
     <div className="z-10 mb-20 flex items-center justify-between rounded-md border-4 border-HeaderOutline p-3 sm:gap-20 lg:gap-80">
-      <img src={logo} alt="Game Logo" />
+      <img src={logo} alt="Game Logo" className="sm:w-32 lg:size-auto" />
       <ScoreDisplay />
     </div>
   );
